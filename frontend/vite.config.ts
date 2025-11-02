d9ec3784a94581f94e860d459c49873c69891d69
ArehmanSiddiqi <arehmansiddiqi@live.com>
2025-11-02 19:13:10 +0500

    Update Config for Build Command

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import react from '@vitejs/plugin-react' 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/static/frontend/',
   build: {
-    outDir: path.resolve(__dirname, '../backend/static/frontend'),
+ outDir: './dist',  
     emptyOutDir: true,
   },
   server: {
