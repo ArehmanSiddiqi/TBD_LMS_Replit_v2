dc6fc7c6ea331efae948b7fdab09160672b5be74
ArehmanSiddiqi <arehmansiddiqi@live.com>
2025-11-02 19:51:25 +0500

    sdfsdf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/frontend/',
+  base: '/',
+  // base: '/static/frontend/',
   build: {
  outDir: './dist',  
     emptyOutDir: true,
